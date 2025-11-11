3fc5319e239833233e3b1a27d0eec0950148edda
Vladimir <v.yaroshenko@techflower.io>
2025-11-11 22:16:08 +0200

    add theme light & dark mode

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:full_food_delivery/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'page/login_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      theme: context.watch<ThemeProvider>().themeData,
+    );
+  }
+}
