be9d0bd7997280c3bae90c7dde01db9802aaeab0
Vladimir <v.yaroshenko@techflower.io>
2026-02-14 18:51:05 +0200

    add firebase cli and add packages core, auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:full_food_delivery/firebase_options.dart';
 import 'package:full_food_delivery/models/restaurant.dart';
 import 'package:full_food_delivery/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'auth/login_or_register.dart';
+import 'services/auth/login_or_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
 
   runApp(
